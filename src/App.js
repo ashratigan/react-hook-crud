f799557361a811cff87a0bb2cc30d71a99ff0901
Ashley <ashley.ratigan@gmail.com>
2019-07-02 18:10:17 -0400

    Followed tutorial on react hooks to make this CRUD app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UserTable from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
 
 const App = () => {
-  return (
-    <div className="container">
-      <h1>CRUD App with Hooks</h1>
-      <div className="flex-row">
-        <div className="flex-large">
-          <h2>Add user</h2>
+    const usersData = [
+        { id: 1, name: 'Ash', username: 'ashes' },
+        { id: 2, name: 'Floyd', username: 'FlyinFloyd' },
+        { id: 3, name: 'Luna', username: 'loopyLuna' },
+    ]
+
+    const [users, setUsers] = useState(usersData)
+    
+    const addUser = user => {
+        user.id = users.length + 1
+        setUsers([...users, user])
+    }
+
+    const deleteUser = id => {
+        setUsers(users.filter(user => user.id !== id))
+        setEditing(false)
+    }
+
+    const [editing, setEditing] = useState(false)
+
+    const initialFormState = { id: null, name: '', username: '' }
+
+    const [currentUser, setCurrentUser] = useState(initialFormState)
+
+    const editRow = user => {
+        setEditing(true)
+        
+        setCurrentUser({ id: user.id, name: user.name, username: user.username})
+    }
+
+    const updateUser = (id, updatedUser) => {
+        setEditing(false)
+
+        setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+    }
+
+    return (
+        <div className="container">
+            <h1>CRUD App with Hooks</h1>
+            <div className="flex-row">
+            <div className="flex-large">
+                {editing ? (
+                    <div>
+                        <h2>Edit user</h2>
+                        <EditUserForm 
+                            editing={editing}
+                            setEditing={setEditing}
+                            currentUser={currentUser}
+                            updateUser={updateUser}
+                        />
+                    </div>
+                ) : (
+                    <div>
+                        <h2>Add user</h2>
+                        <AddUserForm addUser={addUser} />
+                    </div>
+                )}
+            </div>
+            <div className="flex-large">
+                <h2>View users</h2>
+                <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+            </div>
+            </div>
         </div>
-        <div className="flex-large">
-          <h2>View users</h2>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default App
